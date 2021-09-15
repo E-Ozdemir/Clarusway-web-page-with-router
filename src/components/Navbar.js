201cabc2089d022aa4272a262d48c41ed77a73fd
E-Ozdemir <emreclarusway@gmail.com>
2021-09-15 19:03:05 +0200

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,15 +23,6 @@ const Navbar = () => {
             </li>
           </ul>
         </nav>
-        <nav style={{display:'flex', justifyContent:'space-between', alignItems:'center',backgroundColor:'#35424A',padding:'10px 20px'}}>
-          <h2>
-            <span>Subscribe To Our Newsletter</span>
-          </h2>
-          <div>
-            <input type="email" placeholder="E-Mail" />
-            <button>Subscribe</button>
-          </div>
-        </nav>
       </div>
   );
 };
