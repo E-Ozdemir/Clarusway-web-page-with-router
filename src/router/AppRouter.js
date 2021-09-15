ee82abe49a7ac8e74a8c552446137e6eed9fe612
E-Ozdemir <emreclarusway@gmail.com>
2021-09-15 19:04:31 +0200

    approuter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom'
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 import Home from '../pages/Home'
 import About from '../pages/About'
 import Services from '../pages/Services'
@@ -12,7 +12,6 @@ import Css from '../pages/Css'
 const AppRouter = () => {
   return (
     <Router>
-        <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
